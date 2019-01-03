9d7f39e217ebf3ac3a20f2e0fcead5410cfc886d
mhamzajutt96 <m_hamza_jutt@yahoo.com>
2019-01-03 11:53:59 +0000

    Added Production Setto=ing for amazon

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+  
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
