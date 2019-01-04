c270a6f261806d2321448b382aad4173600d5799
mhamzajutt96 <m_hamza_jutt@yahoo.com>
2019-01-04 12:12:35 +0000

    Changing for the Project Champion Paypal Setting

--- app/models/projectchampion.rb
+++ app/models/projectchampion.rb
@@ -10,7 +10,7 @@ class Projectchampion < ApplicationRecord
       cmd: "_xclick",
       upload: 1,
       return: "#{ENV['app_host']}#{return_path}",
-      invoice: self.id,
+      invoice: self.createinvoicecode,
       amount: self.projectchampionpaidamount,
       item_name: self.campaign.title,
       item_number: self.id,
@@ -20,4 +20,11 @@ class Projectchampion < ApplicationRecord
     "#{ENV['paypal_host']}/cgi-bin/webscr?" + values.to_query
   end
   
+  
+  def createinvoicecode
+    @amount = self.projectchampionpaidamount
+    @amount = SecureRandom.urlsafe_base64(8)
+    return @amount
+  end
+    
 end
